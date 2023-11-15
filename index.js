bd37707150f82b5f108d31397885f4db23deda3f
unknown <Rafidah>
2023-11-15 13:30:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const userrouter = require('./router/users')
+const cors = require('cors')
+
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(userrouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
